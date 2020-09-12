7ee75a6932978758324b4a40580a73d73c666892
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2020-09-12 03:02:18 +0300

    Get Order String
    getOrderString in Utils
    
    Static method that returns the order of an element based on the index. The order is described as text e.g. "1st", "7th".

--- app/src/main/java/com/tam/isave/Utils.java
+++ app/src/main/java/com/tam/isave/Utils.java
@@ -21,4 +21,37 @@ public class Utils {
     public static boolean isZeroDouble(double value) {
         return (value <= ZERO_DOUBLE) && (value >= -ZERO_DOUBLE);
     }
+
+    // Returns a String describing the order of the specified @index.
+    // ex: @index 0 - returns "1st"
+    // ex: @index 5 - returns "6th"
+    // ex: @index 22 - returns "23rd"
+    public static String getOrderString(int index) {
+        if(index < 0) { return null; }
+
+        // Increment index to describe order numerically.
+        // E.g. Element 0 is order 1.
+        String orderString = "" + (++index);
+
+        // Last digit of index which will be checked to add to the ending of orderString.
+        // Will be -1 in case the last digit of index is preceded by a 1.
+        // In this case, orderString will end in "th".
+        int indexLastDigit = ( ( (index % 100) / 10 ) != 1 ) ? index % 10 : -1;
+        switch(indexLastDigit) {
+            case 1:
+                orderString += "st";
+                break;
+            case 2:
+                orderString += "nd";
+                break;
+            case 3:
+                orderString += "rd";
+                break;
+            default:
+                orderString += "th";
+                break;
+        }
+
+        return orderString;
+    }
 }
