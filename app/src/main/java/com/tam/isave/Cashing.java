2ab07d1777c629e8bac34988675f3608701c3f5b
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2020-09-14 21:44:53 +0300

    Removed unnecessary "isSalary" constructor param

--- app/src/main/java/com/tam/isave/Cashing.java
+++ app/src/main/java/com/tam/isave/Cashing.java
@@ -2,7 +2,7 @@ package com.tam.isave;
 
 public class Cashing extends Transaction {
 
-    public Cashing(String name, Date date, double value, boolean isSalary) {
+    public Cashing(String name, Date date, double value) {
         super(name, value, date);
     }
 
