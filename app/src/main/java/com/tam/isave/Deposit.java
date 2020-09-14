8ec5eee12643e55ceeccf51b7bea58c9c0772d54
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2020-09-14 21:00:03 +0300

    Deposit name changed to Vault

--- app/src/main/java/com/tam/isave/Deposit.java
+++
@@ -1,8 +0,0 @@
-package com.tam.isave;
-
-public class Deposit extends Category {
-
-    public Deposit(String name, double goal, boolean hasFlexibleGoal) {
-        super(name, goal, false);
-    }
-}
---
+++ app/src/main/java/com/tam/isave/Vault.java
@@ -0,0 +1,8 @@
+package com.tam.isave;
+
+public class Vault extends Category {
+
+    public Vault(String name, double goal, boolean hasFlexibleGoal) {
+        super(name, goal, false);
+    }
+}
