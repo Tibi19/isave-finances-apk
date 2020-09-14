6aad11930ad75227ef7bd21a5d2779a039c7a112
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2020-09-14 21:35:26 +0300

    Removed unnecessary bool "isSalary""

--- app/src/main/java/com/tam/isave/Cashing.java
+++ app/src/main/java/com/tam/isave/Cashing.java
@@ -2,18 +2,8 @@ package com.tam.isave;
 
 public class Cashing extends Transaction {
 
-    private boolean isSalary;
-
     public Cashing(String name, Date date, double value, boolean isSalary) {
         super(name, value, date);
-        this.isSalary = isSalary;
     }
 
-    public boolean isSalary() {
-        return isSalary;
-    }
-
-    public void setSalary(boolean salary) {
-        isSalary = salary;
-    }
 }
