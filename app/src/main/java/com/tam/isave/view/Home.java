1a545a3b931abc723990458d1c1544c27dc8b3dd
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2020-10-25 17:56:44 +0200

    Started home ui

---
+++ app/src/main/java/com/tam/isave/view/Home.java
@@ -0,0 +1,16 @@
+package com.tam.isave.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tam.isave.R;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
