4f2afae84f0608f80281788b50845ae1c38bfdf3
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2021-01-30 18:53:21 +0200

    Constants class

---
+++ app/src/main/java/com/tam/isave/utils/Constants.java
@@ -0,0 +1,17 @@
+package com.tam.isave.utils;
+
+import androidx.annotation.NonNull;
+
+public class Constants {
+
+    public final static String TABLE_NAME_CATEGORY = "category-table";
+    //public final static String TABLE_NAME_CATEGORY_HISTORY = "category-history-table";
+
+    public final static String COLUMN_NAME = "name";
+    public final static String COLUMN_SPENT = "spent";
+    public final static String COLUMN_GOAL = "goal";
+    public final static String COLUMN_GOAL_MODIFIER = "goal-modifier";
+    public final static String COLUMN_GOAL_PASSED = "goal-passed";
+    public final static String COLUMN_HAS_FLEXIBLE_GOAL = "has-flexible-goal";
+
+}
