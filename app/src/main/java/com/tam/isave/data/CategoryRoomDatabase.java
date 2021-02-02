25a1b995baf853928a464ed53ab6598f874c285d
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2021-02-03 00:16:58 +0200

    Created RoomDatabase

---
+++ app/src/main/java/com/tam/isave/data/CategoryRoomDatabase.java
@@ -0,0 +1,39 @@
+package com.tam.isave.data;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.tam.isave.model.CategoryTools.Category;
+import com.tam.isave.utils.Constants;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Category.class}, version = 1)
+public abstract class CategoryRoomDatabase extends RoomDatabase {
+
+    private static volatile CategoryRoomDatabase INSTANCE;
+    public abstract CategoryDao categoryDao();
+
+    private static final int NUMBER_OF_THREADS = 4;
+    public static final ExecutorService DATABASE_WRITE_EXECUTOR =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    public static CategoryRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE != null) return INSTANCE;
+
+        synchronized (CategoryRoomDatabase.class) {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                        CategoryRoomDatabase.class, Constants.DATABASE_NAME)
+                        .build();
+            }
+        }
+
+        return INSTANCE;
+    }
+
+}
