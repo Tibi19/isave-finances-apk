f2c05a9352da502b4c9dc33471890827d8248237
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2021-02-03 00:18:21 +0200

    Rename constants for SQL

--- app/src/main/java/com/tam/isave/utils/Constants.java
+++ app/src/main/java/com/tam/isave/utils/Constants.java
@@ -4,14 +4,16 @@ import androidx.annotation.NonNull;
 
 public class Constants {
 
-    public final static String TABLE_NAME_CATEGORY = "category-table";
-    public final static String TABLE_NAME_HISTORY = "history-table";
+    public final static String DATABASE_NAME = "category_database";
+
+    public final static String TABLE_NAME_CATEGORY = "category_table";
+    public final static String TABLE_NAME_HISTORY = "history_table";
 
     public final static String COLUMN_NAME = "name";
     public final static String COLUMN_SPENT = "spent";
     public final static String COLUMN_GOAL = "goal";
-    public final static String COLUMN_GOAL_MODIFIER = "goal-modifier";
-    public final static String COLUMN_GOAL_PASSED = "goal-passed";
-    public final static String COLUMN_HAS_FLEXIBLE_GOAL = "has-flexible-goal";
+    public final static String COLUMN_GOAL_MODIFIER = "goal_modifier";
+    public final static String COLUMN_GOAL_PASSED = "goal_passed";
+    public final static String COLUMN_HAS_FLEXIBLE_GOAL = "has_flexible_goal";
 
 }
