327089080b795c5da1e0756fede6bdf534b11d9e
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2021-02-03 00:17:14 +0200

    Created Category DAO

---
+++ app/src/main/java/com/tam/isave/data/CategoryDao.java
@@ -0,0 +1,30 @@
+package com.tam.isave.data;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.tam.isave.model.CategoryTools.Category;
+import com.tam.isave.utils.Constants;
+
+import java.util.List;
+
+@Dao
+public interface CategoryDao {
+
+    @Insert
+    void insert(Category category);
+
+    @Update
+    void update(Category category);
+
+    @Delete
+    void delete(Category category);
+
+    @Query("SELECT * FROM category_table")
+    LiveData<List<Category>> getCategories();
+
+}
