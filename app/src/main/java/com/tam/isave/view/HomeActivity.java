23208dd2e4d17f099da0e4b1b95b4ad3f29733ac
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2021-02-08 22:50:10 +0200

    Testing add category
    - setup recyclerview
    - setup livedata observer
    - setup onclick
    - testing add category functionality with random categories
    - menu button deletes all categories for testing

--- app/src/main/java/com/tam/isave/view/HomeActivity.java
+++ app/src/main/java/com/tam/isave/view/HomeActivity.java
@@ -1,14 +1,24 @@
 package com.tam.isave.view;
 
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 import com.tam.isave.R;
+import com.tam.isave.adapter.CategoryAdapter;
+import com.tam.isave.model.CategoryTools.Category;
+import com.tam.isave.utils.DebugUtils;
+import com.tam.isave.viewmodel.CategoryViewModel;
 
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,15 +26,78 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class HomeActivity extends AppCompatActivity {
+import java.util.List;
 
-    private AppBarConfiguration mAppBarConfiguration;
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private RecyclerView categoryRecycler;
+    private CategoryAdapter categoryAdapter;
+    private CategoryViewModel categoryViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        initCategoryController();
+        setupCategoryRecycler();
+        setupCategoryViewModel();
     }
 
+    private void setupCategoryRecycler() {
+        // Instantiate recyclerview and its adapter.
+        categoryRecycler = findViewById(R.id.recycler_category);
+        categoryAdapter = new CategoryAdapter(this);
+        // Set recycler's adapter.
+        categoryRecycler.setAdapter(categoryAdapter);
+        // Set recycler's layout manager.
+        categoryRecycler.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void setupCategoryViewModel() {
+        categoryViewModel = new ViewModelProvider(this).get(CategoryViewModel.class);
+        // Observer
+        categoryViewModel.getCategories().observe(this, new Observer<List<Category>>() {
+            @Override
+            public void onChanged(List<Category> categories) {
+                categoryAdapter.setCategories(categories);
+            }
+        });
+    }
+
+    private void initCategoryController(){
+        // Set functionality of add category button.
+        findViewById(R.id.button_add_category).setOnClickListener(this);
+        findViewById(R.id.button_home_menu).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.button_add_category:
+                addCategory();
+                break;
+            case R.id.button_home_menu:
+                // DEBUGGING: Menu button deletes all instances of category from database.
+                categoryViewModel.deleteAll();
+                break;
+        }
+    }
+
+    private void addCategory() {
+        DebugUtils.makeToast(this, "added category");
+        Category category = getRandomCategory();
+        categoryViewModel.insert(category);
+    }
+
+    private Category getRandomCategory() {
+        int randomGoal = DebugUtils.getRandomIntInRange(150, 800);
+        Category category = new Category(DebugUtils.getRandomCategoryName(), randomGoal);
+        category.setSpent(DebugUtils.getRandomDouble(randomGoal));
+
+        return category;
+    }
 }
