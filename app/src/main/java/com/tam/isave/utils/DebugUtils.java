e9d06872d79e833350e87a99c96b482e1d0b1862
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2021-02-08 22:44:29 +0200

    Created DebugUtils

---
+++ app/src/main/java/com/tam/isave/utils/DebugUtils.java
@@ -0,0 +1,44 @@
+package com.tam.isave.utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class DebugUtils {
+
+    public static String categoryNames[] = {"Groceries", "Toys", "Clothing", "Sweets", "Notebooks", "Extra", "Health",
+            "Hygiene", "Learning", "Self-Development", "Others", "Going out", "Take out", "Tobacco"};
+    public static Random random = new Random();
+
+    public static void makeToast(Context context, String text) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+    }
+
+    public static String getRandomCategoryName() {
+        return categoryNames[random.nextInt(categoryNames.length)];
+    }
+
+    /**
+     * Generates a random double between [0, maxValue).
+     * @param maxValue The maximum value.
+     * @return a value between [0, maxValue)
+     */
+    public static double getRandomDouble(int maxValue) {
+        return getRandomInt(maxValue) + random.nextDouble();
+    }
+
+    /**
+     * Generates random int from 0 to maxValue.
+     * @param maxValue The maximum value.
+     * @return a value between 0 and maxvalue.
+     */
+    public static int getRandomInt(int maxValue) {
+        return random.nextInt(maxValue + 1);
+    }
+
+    public static int getRandomIntInRange(int minValue, int maxValue) {
+        int rangeMaxValue = maxValue - minValue + 1;
+        return minValue + random.nextInt(rangeMaxValue);
+    }
+}
