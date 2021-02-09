861e9892a80deb7141e3fdcbc6a5effd0873b4c4
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2021-02-09 22:50:50 +0200

    Update constants to reflect new cleaned up model
    - has flexible goal is now just flexible goal
    - new table version becuase of the aboce change, affecting the old table's schema

--- app/src/main/java/com/tam/isave/utils/Constants.java
+++ app/src/main/java/com/tam/isave/utils/Constants.java
@@ -6,6 +6,7 @@ public class Constants {
 
     public final static String DATABASE_NAME = "category_database";
 
+    public static final int VERSION_CATEGORY_TABLE = 2;
     public final static String TABLE_NAME_CATEGORY = "category_table";
     public final static String TABLE_NAME_HISTORY = "history_table";
 
@@ -14,6 +15,5 @@ public class Constants {
     public final static String COLUMN_GOAL = "goal";
     public final static String COLUMN_GOAL_MODIFIER = "goal_modifier";
     public final static String COLUMN_GOAL_PASSED = "goal_passed";
-    public final static String COLUMN_HAS_FLEXIBLE_GOAL = "has_flexible_goal";
-
+    public final static String COLUMN_FLEXIBLE_GOAL = "flexible_goal";
 }
