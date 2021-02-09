f47ea1c2eb72b9223c24c15f603ebd07cb0f10bf
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2021-02-09 23:51:44 +0200

    Clean up

--- app/src/main/java/com/tam/isave/model/CategoryTools/Interval.java
+++ app/src/main/java/com/tam/isave/model/CategoryTools/Interval.java
@@ -2,19 +2,17 @@ package com.tam.isave.model.CategoryTools;
 
 public class Interval extends Category {
 
-    private static final String DEFAULT_NAME = "interval";
+    private static final String DEFAULT_NAME = "Interval";
 
     private int days; // Number of days in this interval.
-    private int id; // Should represent object's index in parent's array of Intervals.
 
     public Interval(int id, int days, double goal) {
-        super(DEFAULT_NAME + id, goal);
+        super(DEFAULT_NAME + (id + 1), goal);
         this.days = days;
-        this.id = id;
     }
 
     public void modify(double spent, double goal, GoalAdapter adapter) {
-        super.modify(DEFAULT_NAME + id, spent, goal, true, adapter);
+        super.modify(getName(), spent, goal, true, adapter);
     }
 
     public int getDays() {
