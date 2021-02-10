8be30a8171ef65f77a86e444ea4a9a95eaf723b9
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2021-02-10 23:37:12 +0200

    Removed redundant IProgressDisplayable

--- app/src/main/java/com/tam/isave/model/IProgressDisplayable.java
+++
@@ -1,9 +0,0 @@
-package com.tam.isave.model;
-
-// To be used for UI.
-public interface IProgressDisplayable {
-
-    String getInfoAboutProgress(); // Needs to offer information about spending progress.
-    String getProgress(); // The spending progress.
-
-}
