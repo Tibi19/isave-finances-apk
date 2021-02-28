a37dd14928ddb468fbb45f2c2818a84952acdb8e
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2021-02-28 18:11:03 +0200

    Added categories fragment

---
+++ app/src/main/java/com/tam/isave/view/CategoriesFragment.java
@@ -0,0 +1,94 @@
+package com.tam.isave.view;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.tam.isave.adapter.CategoryAdapter;
+import com.tam.isave.databinding.FragmentCategoriesBinding;
+import com.tam.isave.databinding.PopupAddCategoryBinding;
+import com.tam.isave.viewmodel.CategoryViewModel;
+import com.tam.isave.R;
+
+public class CategoriesFragment extends Fragment {
+
+    private FragmentCategoriesBinding binding;
+    private CategoryAdapter categoryAdapter;
+    private CategoryViewModel categoryViewModel;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentCategoriesBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setupCategoryController();
+        setupCategoryRecycler();
+        setupCategoryViewModel();
+    }
+
+    private void setupCategoryRecycler() {
+        // Instantiate recyclerview and its adapter.
+        RecyclerView categoryRecycler = binding.recyclerCategory;
+        categoryAdapter = new CategoryAdapter(getContext());
+        // Set recycler's adapter.
+        categoryRecycler.setAdapter(categoryAdapter);
+        categoryRecycler.setAdapter(categoryAdapter);
+        // Set recycler's layout manager.
+        categoryRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+    private void setupCategoryViewModel() {
+        categoryViewModel = new ViewModelProvider(this).get(CategoryViewModel.class);
+        // Observer
+        categoryViewModel.getCategories().observe(getViewLifecycleOwner(), categories ->
+                categoryAdapter.setCategories(categories));
+    }
+
+    private void setupCategoryController(){
+        // Functionality of add category button
+        binding.buttonAddCategory.setOnClickListener(listener -> showAddCategoryPopup());
+        // Set functionality of menu button, current functionality is only for testing purposes.
+        getActivity().findViewById(R.id.button_home_menu).setOnClickListener(listener -> categoryViewModel.deleteAll());
+    }
+
+    private void showAddCategoryPopup() {
+        AlertDialog addCategoryDialog;
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        PopupAddCategoryBinding addCategoryBinding = PopupAddCategoryBinding.inflate(getLayoutInflater());
+
+        builder.setView(addCategoryBinding.getRoot());
+        addCategoryDialog = builder.create();
+        addCategoryDialog.setCancelable(true);
+        addCategoryDialog.getWindow().setGravity(Gravity.BOTTOM);
+
+        addCategoryBinding.buttonAddCategoryCancel.setOnClickListener(listener -> addCategoryDialog.dismiss());
+        addCategoryBinding.buttonAddCategorySubmit.setOnClickListener(listener -> {
+            addNewCategory(addCategoryBinding);
+            addCategoryDialog.dismiss();
+        });
+
+        addCategoryDialog.show();
+    }
+
+    private void addNewCategory(PopupAddCategoryBinding addCategoryBinding) {
+        boolean categoryFlexibility = addCategoryBinding.checkAddCategoryIsFlexible.isChecked();
+        String categoryName = addCategoryBinding.editAddCategoryName.getText().toString();
+        double categoryGoal = Double.parseDouble(addCategoryBinding.editAddCategoryBudget.getText().toString());
+        categoryViewModel.addCategory(categoryName, categoryGoal, categoryFlexibility);
+    }
+}
--- app/src/main/java/com/tam/isave/view/HomeActivity.java
+++ app/src/main/java/com/tam/isave/view/HomeActivity.java
@@ -1,25 +1,13 @@
 package com.tam.isave.view;
 
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.Window;
-
-import com.tam.isave.adapter.CategoryAdapter;
 import com.tam.isave.databinding.ActivityHomeBinding;
-import com.tam.isave.databinding.PopupAddCategoryBinding;
-import com.tam.isave.viewmodel.CategoryViewModel;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 
 public class HomeActivity extends AppCompatActivity {
 
-    private CategoryAdapter categoryAdapter;
-    private CategoryViewModel categoryViewModel;
     private ActivityHomeBinding homeBinding;
 
     @Override
@@ -28,78 +16,16 @@ public class HomeActivity extends AppCompatActivity {
         homeBinding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(homeBinding.getRoot());
 
-        initCategoryController();
-        setupCategoryRecycler();
-        setupCategoryViewModel();
+        if (savedInstanceState == null) {
+            startCategoriesTransaction();
+        }
     }
 
-    private void setupCategoryRecycler() {
-        // Instantiate recyclerview and its adapter.
-        RecyclerView categoryRecycler = homeBinding.recyclerCategory;
-        categoryAdapter = new CategoryAdapter(this);
-        // Set recycler's adapter.
-        categoryRecycler.setAdapter(categoryAdapter);
-        categoryRecycler.setAdapter(categoryAdapter);
-        // Set recycler's layout manager.
-        categoryRecycler.setLayoutManager(new LinearLayoutManager(this));
+    private void startCategoriesTransaction() {
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(homeBinding.fragmentContainerCategories.getId(), CategoriesFragment.class, null)
+                .commit();
     }
 
-    private void setupCategoryViewModel() {
-        categoryViewModel = new ViewModelProvider(this).get(CategoryViewModel.class);
-        // Observer
-        categoryViewModel.getCategories().observe(this, categories ->
-                categoryAdapter.setCategories(categories));
-    }
-
-    private void initCategoryController(){
-        // Set functionality of menu button, current functionality is only for testing purposes.
-        homeBinding.buttonHomeMenu.setOnClickListener(listener -> deleteAllCategories());
-        // Functionality of add category button
-        homeBinding.buttonAddCategory.setOnClickListener(listener -> showAddCategoryPopup());
-    }
-
-    private void deleteAllCategories() {
-        categoryViewModel.deleteAll();
-    }
-
-//    private void addCategory() {
-//        DebugUtils.makeToast(this, "added category");
-//        Category category = getRandomCategory();
-//        categoryViewModel.addCategory(category.getName(), category.getGoal(), category.isFlexibleGoal());
-//    }
-
-//    private Category getRandomCategory() {
-//        int randomGoal = DebugUtils.getRandomIntInRange(150, 800);
-//        Category category = new Category(DebugUtils.getRandomCategoryName(), randomGoal);
-//        category.setSpent(DebugUtils.getRandomDouble(randomGoal));
-//
-//        return category;
-//    }
-
-    private void showAddCategoryPopup() {
-        AlertDialog addCategoryDialog;
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        PopupAddCategoryBinding addCategoryBinding = PopupAddCategoryBinding.inflate(getLayoutInflater());
-
-        builder.setView(addCategoryBinding.getRoot());
-        addCategoryDialog = builder.create();
-        addCategoryDialog.setCancelable(true);
-        Window addCategoryWindow = addCategoryDialog.getWindow();
-        addCategoryDialog.getWindow().setGravity(Gravity.BOTTOM);
-
-        addCategoryBinding.buttonAddCategoryCancel.setOnClickListener(listener -> addCategoryDialog.dismiss());
-        addCategoryBinding.buttonAddCategorySubmit.setOnClickListener(listener -> {
-            addNewCategory(addCategoryBinding);
-            addCategoryDialog.dismiss();
-        });
-
-        addCategoryDialog.show();
-    }
-
-    private void addNewCategory(PopupAddCategoryBinding addCategoryBinding) {
-        boolean categoryFlexibility = addCategoryBinding.checkAddCategoryIsFlexible.isChecked();
-        String categoryName = addCategoryBinding.editAddCategoryName.getText().toString();
-        double categoryGoal = Double.parseDouble(addCategoryBinding.editAddCategoryBudget.getText().toString());
-        categoryViewModel.addCategory(categoryName, categoryGoal, categoryFlexibility);
-    }
 }
