37c4da47264535194e1494648c1e6710c4a58fe4
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2021-03-14 17:39:50 +0200

    Added constants for Transaction Database
    Also modified the name of the variable storing the category database name

--- app/src/main/java/com/tam/isave/utils/Constants.java
+++ app/src/main/java/com/tam/isave/utils/Constants.java
@@ -4,16 +4,27 @@ import androidx.annotation.NonNull;
 
 public class Constants {
 
-    public final static String DATABASE_NAME = "category_database";
+    // Category Database Constants
+    public final static String CATEGORY_DATABASE = "category_database";
 
     public static final int VERSION_CATEGORY_TABLE = 2;
     public final static String TABLE_NAME_CATEGORY = "category_table";
-    public final static String TABLE_NAME_HISTORY = "history_table";
 
-    public final static String COLUMN_NAME = "name";
-    public final static String COLUMN_SPENT = "spent";
-    public final static String COLUMN_GOAL = "goal";
-    public final static String COLUMN_GOAL_MODIFIER = "goal_modifier";
-    public final static String COLUMN_GOAL_PASSED = "goal_passed";
-    public final static String COLUMN_FLEXIBLE_GOAL = "flexible_goal";
+    public final static String CATEGORY_COLUMN_NAME = "name";
+    public final static String CATEGORY_COLUMN_SPENT = "spent";
+    public final static String CATEGORY_COLUMN_GOAL = "goal";
+    public final static String CATEGORY_COLUMN_GOAL_MODIFIER = "goal_modifier";
+    public final static String CATEGORY_COLUMN_GOAL_PASSED = "goal_passed";
+    public final static String CATEGORY_COLUMN_FLEXIBLE_GOAL = "flexible_goal";
+
+    // Transaction Database Constants
+    public final static String TRANSACTION_DATABASE = "transaction_database";
+
+    public static final int VERSION_TRANSACTION_TABLE = 1;
+    public final static String TABLE_NAME_TRANSACTION = "transaction_table";
+
+    public final static String TRANSACTION_COLUMN_NAME = "name";
+    public final static String TRANSACTION_COLUMN_VALUE = "value";
+    public final static String TRANSACTION_COLUMN_DATE_VALUE = "date_value";
+    public final static String TRANSACTION_COLUMN_PARENT_ID = "parent_id";
 }
