62c929513f3c57751dcbdf2dd11836e9c8c81258
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2021-03-14 17:40:17 +0200

    Added Transaction Database

---
+++ app/src/main/java/com/tam/isave/data/TransactionRoomDatabase.java
@@ -0,0 +1,40 @@
+package com.tam.isave.data;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.tam.isave.model.transaction.Transaction;
+import com.tam.isave.utils.Constants;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Transaction.class}, version = Constants.VERSION_TRANSACTION_TABLE, exportSchema = false)
+public abstract class TransactionRoomDatabase extends RoomDatabase {
+
+    private static volatile TransactionRoomDatabase INSTANCE;
+    public abstract TransactionDao transactionDao();
+
+    private static final int NUMBER_OF_THREADS = 4;
+    public static final ExecutorService DATABASE_WRITE_EXECUTOR =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    public static TransactionRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE != null) return INSTANCE;
+
+        synchronized (TransactionRoomDatabase.class) {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                        TransactionRoomDatabase.class, Constants.TRANSACTION_DATABASE)
+                        .fallbackToDestructiveMigration()
+                        .build();
+            }
+        }
+
+        return INSTANCE;
+    }
+
+}
