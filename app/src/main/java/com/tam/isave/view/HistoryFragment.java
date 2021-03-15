bfa9fec4719c6fbb5c6dd48e5d36523811e3e1fa
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2021-03-15 23:40:10 +0200

    Created History Fragment
    - History fragment class TODO

---
+++ app/src/main/java/com/tam/isave/view/HistoryFragment.java
@@ -0,0 +1,15 @@
+package com.tam.isave.view;
+
+import androidx.fragment.app.Fragment;
+
+import com.tam.isave.adapter.HistoryAdapter;
+import com.tam.isave.databinding.FragmentHistoryBinding;
+import com.tam.isave.viewmodel.TransactionViewModel;
+
+public class HistoryFragment extends Fragment {
+
+    private FragmentHistoryBinding binding;
+    private HistoryAdapter historyAdapter;
+    private TransactionViewModel transactionViewModel;
+
+}
