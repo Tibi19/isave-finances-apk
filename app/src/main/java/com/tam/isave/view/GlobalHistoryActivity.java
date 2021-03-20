4d9d958a18f07b6796b5c35eba3d7060c959d22e
Tibi19 <44575542+Tibi19@users.noreply.github.com>
2021-03-20 12:43:40 +0200

    Created global history Activity

--- app/src/main/java/com/tam/isave/view/GlobalHistoryActivity.java
+++ app/src/main/java/com/tam/isave/view/GlobalHistoryActivity.java
@@ -1,4 +1,34 @@
 package com.tam.isave.view;
 
-public class GlobalHistoryActivity {
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tam.isave.databinding.ActivityGlobalHistoryBinding;
+import com.tam.isave.utils.Constants;
+import com.tam.isave.utils.HistoryIdentifier;
+
+public class GlobalHistoryActivity extends AppCompatActivity {
+
+    private ActivityGlobalHistoryBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityGlobalHistoryBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        startHistoryTransaction();
+    }
+
+    private void startHistoryTransaction() {
+        Bundle bundle = new Bundle();
+        bundle.putString(Constants.KEY_HISTORY_TYPE, HistoryIdentifier.HISTORY_TYPE_GLOBAL);
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(binding.fragmentContainerHistory.getId(), HistoryFragment.class, bundle)
+                .commit();
+    }
 }
