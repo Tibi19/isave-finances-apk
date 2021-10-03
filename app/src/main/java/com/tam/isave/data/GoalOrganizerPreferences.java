bc9a26f8cacee379cd83b3d4aa8bbeab4cb5e95a
tibi19 <tiberiu.manescu@gmail.com>
2021-10-03 16:48:14 +0300

    GoalOrganizer shared preferences

---
+++ app/src/main/java/com/tam/isave/data/GoalOrganizerPreferences.java
@@ -0,0 +1,45 @@
+package com.tam.isave.data;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.tam.isave.model.goalorganizer.GoalOrganizer;
+import com.tam.isave.utils.Constants;
+
+public class GoalOrganizerPreferences {
+
+    private SharedPreferences sharedPreferences;
+
+    public GoalOrganizerPreferences(Application application) {
+        sharedPreferences = application.getSharedPreferences(Constants.PREFERENCES_FILE_KEY, Context.MODE_PRIVATE);
+    }
+
+    public double readGlobalGoal() {
+        return sharedPreferences.getFloat(Constants.PREFERENCES_KEY_GLOBAL_GOAL, Constants.PREFERENCES_DEFAULT_FLOAT);
+    }
+
+    public int readGlobalDays() {
+        return sharedPreferences.getInt(Constants.PREFERENCES_KEY_GLOBAL_DAYS, Constants.PREFERENCES_DEFAULT_INT);
+    }
+
+    public int readFirstDayValue() {
+        return sharedPreferences.getInt(Constants.PREFERENCES_KEY_FIRST_DAY_VALUE, Constants.PREFERENCES_DEFAULT_INT);
+    }
+
+    public int readIntervalsCount() {
+        return sharedPreferences.getInt(Constants.PREFERENCES_KEY_INTERVALS_COUNT, Constants.PREFERENCES_DEFAULT_INT);
+    }
+
+    public void saveGlobalOrganizer(GoalOrganizer organizer) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putFloat(Constants.PREFERENCES_KEY_GLOBAL_GOAL, (float) organizer.getGlobalGoal());
+        editor.putInt(Constants.PREFERENCES_KEY_GLOBAL_DAYS, organizer.getGlobalIntervalDays());
+        editor.putInt(Constants.PREFERENCES_KEY_FIRST_DAY_VALUE, organizer.getFirstDay().getValue());
+        editor.putInt(Constants.PREFERENCES_KEY_INTERVALS_COUNT, organizer.getIntervalsNr());
+
+        editor.apply();
+    }
+
+}
