e30f36e5336b26b1801fa061d4f6fe081e130eaf
tibi19 <tiberiu.manescu@gmail.com>
2021-10-04 00:06:39 +0300

    Added binding update and controller setup

--- app/src/main/java/com/tam/isave/view/fragment/GoalOrganizerFragment.java
+++ app/src/main/java/com/tam/isave/view/fragment/GoalOrganizerFragment.java
@@ -1,6 +1,7 @@
 package com.tam.isave.view.fragment;
 
 import android.os.Bundle;
+import android.os.Debug;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,12 +9,18 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.tam.isave.databinding.FragmentOrganizerBinding;
+import com.tam.isave.model.goalorganizer.GoalOrganizer;
+import com.tam.isave.utils.Date;
+import com.tam.isave.utils.DebugUtils;
+import com.tam.isave.viewmodel.GoalOrganizerViewModel;
 
 public class GoalOrganizerFragment extends Fragment {
 
     private FragmentOrganizerBinding binding;
+    private GoalOrganizerViewModel organizerViewModel;
 
     @Nullable
     @Override
@@ -25,5 +32,36 @@ public class GoalOrganizerFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        organizerViewModel = new ViewModelProvider(this).get(GoalOrganizerViewModel.class);
+        updateBinding();
+        setupOrganizerController();
+    }
+
+    private void updateBinding() {
+        GoalOrganizer organizer = organizerViewModel.getGoalOrganizer();
+        binding.tvIntervalDetails.setText(organizer.getIntervalsProgressString());
+        binding.tvDayDetails.setText(organizer.getDaysProgressString());
+        binding.tvOrganizerProgress.setText(organizer.getBudgetProgress());
+    }
+
+    private void setupOrganizerController() {
+        // binding.btnOrganizerHistory.setOnClickListener();
+        binding.btnOrganizerEdit.setOnClickListener(listener -> showEditOrganizerPopup());
+        binding.btnOrganizerReset.setOnClickListener(listener -> deleteOrganizer());
+    }
+
+    private void showEditOrganizerPopup() {
+        // TESTING
+        Date firstDay = Date.today(); // Test with old days next.
+        Date lastDay = firstDay.addDays(DebugUtils.getRandomIntInRange(15, 30));
+        double globalGoal = DebugUtils.getRandomDoubleInRange(500, 5000);
+        int intervalsCount = DebugUtils.getRandomIntInRange(2, 7);
+        organizerViewModel.updateGoalOrganizer(globalGoal, intervalsCount, firstDay, lastDay);
+        updateBinding();
+    }
+
+    private void deleteOrganizer() {
+        organizerViewModel.deleteGoalOrganizer();
+        updateBinding();
     }
 }
