e9e2745b9225af7c320c5d3626b4cfe9e17f229b
Tiberiu Manescu <44575542+Tibi19@users.noreply.github.com>
2021-11-18 00:59:16 +0200

    Add methods for finding first day and last day of an interval

---
+++ app/src/main/java/com/tam/isave/model/goalorganizer/IntervalUtils.java
@@ -0,0 +1,36 @@
+package com.tam.isave.model.goalorganizer;
+
+import com.tam.isave.utils.Date;
+
+public class IntervalUtils {
+
+    public static int getIntervalFirstDayValue(Interval interval, GoalOrganizer organizer) {
+        if(interval == null || organizer == null) { return -1; }
+
+        Date intervalFirstDay = organizer.getFirstDay();
+        for(Interval intervalItem : organizer.getIntervals()) {
+            if(interval.getId() == intervalItem.getId()) { return intervalFirstDay.getValue(); }
+            intervalFirstDay = intervalFirstDay.addDays(intervalItem.getDays());
+        }
+
+        return -1;
+    }
+
+    public static int getIntervalLastDayValue(Interval interval, GoalOrganizer organizer) {
+        if(interval == null || organizer == null) { return -1; }
+
+        Interval[] intervals = organizer.getIntervals();
+        int firstIntervalDays = intervals[0].getDays();
+        Date intervalLastDay = organizer.getFirstDay().countDays(firstIntervalDays);
+
+        for(int i = 0; i < intervals.length; i++) {
+            Interval intervalItem = intervals[i];
+            if(interval.getId() == intervalItem.getId()) { return intervalLastDay.getValue(); }
+            if(i == intervals.length - 1) { return -1; }
+            intervalLastDay = intervalLastDay.addDays(intervals[i + 1].getDays());
+        }
+
+        return -1;
+    }
+
+}
