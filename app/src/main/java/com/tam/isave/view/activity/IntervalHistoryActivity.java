396176c18e27ea4024234aea172c42a56afa22af
Tiberiu Manescu <44575542+Tibi19@users.noreply.github.com>
2021-11-19 01:35:29 +0200

    Add methods to update binding, setting new intervals and replacing the current fragment

--- app/src/main/java/com/tam/isave/view/activity/IntervalHistoryActivity.java
+++ app/src/main/java/com/tam/isave/view/activity/IntervalHistoryActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.tam.isave.databinding.ActivityIntervalHistoryBinding;
@@ -54,26 +55,70 @@ public class IntervalHistoryActivity extends AppCompatActivity {
         else focusInterval = intervals[intervals.length - 1]; // Last day being before today is the only scenario left.
     }
 
-    private void updateHistoryFragment() {
-        if(focusInterval == null) { return; }
-
+    private Bundle getHistoryBundle() {
         Bundle bundle = new Bundle();
         bundle.putString(Constants.KEY_HISTORY_TYPE, HistoryIdentifier.HISTORY_TYPE_INTERVAL);
         bundle.putInt(Constants.KEY_START_DATE_VALUE, IntervalUtils.getIntervalFirstDayValue(focusInterval, organizer));
         bundle.putInt(Constants.KEY_END_DATE_VALUE, IntervalUtils.getIntervalLastDayValue(focusInterval, organizer));
+
+        return bundle;
+    }
+
+    private void updateHistoryFragment() {
+        if(focusInterval == null) { return; }
+
         getSupportFragmentManager().beginTransaction()
                 .setReorderingAllowed(true)
-                .add(binding.fragmentContainerHistory.getId(), HistoryFragment.class, bundle)
+                .add(binding.fragmentContainerHistory.getId(), HistoryFragment.class, getHistoryBundle())
+                .commit();
+    }
+
+    private void replaceHistoryFragment() {
+        if(focusInterval == null) { return; }
+
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .replace(binding.fragmentContainerHistory.getId(), HistoryFragment.class, getHistoryBundle())
                 .commit();
     }
 
     private void updateBinding() {
         if(focusInterval == null) { return; }
+
+        Date intervalFirstDay = new Date(IntervalUtils.getIntervalFirstDayValue(focusInterval, organizer));
+        Date intervalLastDay = new Date(IntervalUtils.getIntervalLastDayValue(focusInterval, organizer));
+        int intervalDays = focusInterval.getDays();
+
+        binding.titleIntervalName.setText(focusInterval.getName());
+        binding.tvBudgetDetails.setText(focusInterval.getDetailedProgress());
+        binding.tvFirstDayDetails.setText(intervalFirstDay.toString());
+        binding.tvLastDayDetails.setText(intervalLastDay.toString());
+        binding.tvDaysDetails.setText(String.valueOf(intervalDays));
     }
 
     private void setupPreviousButton() {
+        binding.buttonPreviousInterval.setOnClickListener(changeFocusToPreviousInterval -> {
+            if(focusInterval == null) { return; }
+
+            Interval previousInterval = IntervalUtils.getPreviousInterval(focusInterval, organizer);
+            if(previousInterval == null) { return; }
+
+            focusInterval = previousInterval;
+            replaceHistoryFragment();
+            updateBinding();
+        });
     }
 
     private void setupNextButton() {
+        binding.buttonNextInterval.setOnClickListener(changeFocusToNextInterval -> {
+            if(focusInterval == null) { return; }
+
+            Interval nextInterval = IntervalUtils.getNextInterval(focusInterval, organizer);
+            if(nextInterval == null) { return; }
+
+            focusInterval = nextInterval;
+            replaceHistoryFragment();
+            updateBinding();
+        });
     }
 }
