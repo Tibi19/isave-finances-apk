2db8f58c8f33444c61ea65856e16ed6e2f0df241
Tiberiu Manescu <44575542+Tibi19@users.noreply.github.com>
2022-02-28 00:35:46 +0200

    Fixed planner hints not being updated

--- app/src/main/java/com/tam/isave/viewmodel/PlannerViewModel.java
+++ app/src/main/java/com/tam/isave/viewmodel/PlannerViewModel.java
@@ -5,11 +5,13 @@ import android.content.Context;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
 
 import com.tam.isave.adapter.PlannerAdapter;
 import com.tam.isave.databinding.PopupCashingBinding;
 import com.tam.isave.model.ModelRepository;
 import com.tam.isave.model.category.Category;
+import com.tam.isave.utils.LiveDataUtils;
 import com.tam.isave.view.dialog.ErrorBuilder;
 
 import java.util.List;
@@ -17,25 +19,30 @@ import java.util.List;
 public class PlannerViewModel extends AndroidViewModel {
 
     private ModelRepository modelRepository;
-    private List<Category> categories;
+    private LiveData<List<Category>> categoriesLiveData;
     private PlannerAdapter plannerAdapter;
 
     public PlannerViewModel(@NonNull Application application) {
         super(application);
         modelRepository = ModelRepository.getModelRepository(application);
-        categories = modelRepository.getCategories().getValue();
+        categoriesLiveData = modelRepository.getCategories();
     }
 
     public void updateCategoriesNewBudgets(boolean addToExistingBudget, boolean shouldResetCategories) {
-        if(shouldResetCategories) { modelRepository.resetAllCategories(); }
+        LiveDataUtils.observeOnce(
+                categoriesLiveData,
+                categories -> {
+                    if(shouldResetCategories) { modelRepository.resetAllCategories(); }
 
-        for(Category category : categories) {
-            String categoryName = category.getName();
-            double newBudget = plannerAdapter.getCategoryPlannedBudget(categoryName);
-            if(newBudget < 0.0) { continue; }
-            if(addToExistingBudget && !shouldResetCategories) { newBudget += category.getGoal(); }
-            modelRepository.modifyCategory(category, categoryName, category.getSpent(), newBudget, category.isFlexibleGoal());
-        }
+                    for(Category category : categories) {
+                        String categoryName = category.getName();
+                        double newBudget = plannerAdapter.getCategoryPlannedBudget(categoryName);
+                        if(newBudget < 0.0) { continue; }
+                        if(addToExistingBudget && !shouldResetCategories) { newBudget += category.getGoal(); }
+                        modelRepository.modifyCategory(category, categoryName, category.getSpent(), newBudget, category.isFlexibleGoal());
+                    }
+                }
+        );
     }
 
     public boolean isCashingValid(PopupCashingBinding cashingPopupBinding, Context context) {
@@ -53,7 +60,7 @@ public class PlannerViewModel extends AndroidViewModel {
         updateCategoriesNewBudgets(false, false);
     }
 
-    public List<Category> getCategories() { return categories; }
+    public List<Category> getCategories() { return categoriesLiveData.getValue(); }
 
     public void setPlannerAdapter(PlannerAdapter plannerAdapter) { this.plannerAdapter = plannerAdapter; }
 
