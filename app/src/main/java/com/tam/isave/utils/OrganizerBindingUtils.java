4d55a4bb98db608e45148b1d38519d02c165f178
Tiberiu Manescu <44575542+Tibi19@users.noreply.github.com>
2022-02-27 23:32:27 +0200

    Added reset organizer binding utils

--- app/src/main/java/com/tam/isave/utils/OrganizerBindingUtils.java
+++ app/src/main/java/com/tam/isave/utils/OrganizerBindingUtils.java
@@ -1,15 +1,26 @@
 package com.tam.isave.utils;
 
+import java.util.function.BiConsumer;
+
 public class OrganizerBindingUtils {
 
     private static Runnable updateOrganizerBinding;
+    private static BiConsumer<Integer, Integer> resetOrganizerBindingIfTimeChange;
 
     public static void setUpdateBinding(Runnable updateBinding) {
         updateOrganizerBinding = updateBinding;
     }
 
+    public static void setResetBindingIfTimeChange(BiConsumer<Integer, Integer> resetBindingIfTimeChange) {
+        resetOrganizerBindingIfTimeChange = resetBindingIfTimeChange;
+    }
+
     public static void updateBinding() {
         updateOrganizerBinding.run();
+    }
+
+    public static void resetBindingIfTimeChange(int originalFirstDayValue, int originalOrganizerDays) {
+        resetOrganizerBindingIfTimeChange.accept(originalFirstDayValue, originalOrganizerDays);
     }
 
 }
